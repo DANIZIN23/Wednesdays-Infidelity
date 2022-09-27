e489dde5a4c0cfd733c6aee04c8791010321fed2
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-09-26 21:08:45 -0300

    Update StoryMenuState.hx

--- source/states/menus/StoryMenuState.hx
+++ source/states/menus/StoryMenuState.hx
@@ -183,7 +183,11 @@ class StoryMenuState extends MusicBeatState
 
 		changeWeek();
 
-		super.create();
+		#if android
+		addVirtualPad(LEFT_FULL, A_B);
+		#end	
+		
+			super.create();
 	}
 
 	override function update(elapsed:Float)
